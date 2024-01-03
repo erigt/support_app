1b29d8a898035b717ec65a83bdc7a596fc1967ff
Erika <146768635+erigt@users.noreply.github.com>
2024-01-03 21:52:16 +0100

    Readme

---
+++ backend/src/test/java/dev/erika/support_app/SupportAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.erika.support_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
